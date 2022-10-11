93c0f07c5dd1fa4822c55d8fc8e8890d6604f9c1
Reander Agulto <reanderagulto29@gmail.com>
2022-10-11 14:15:06 +0800

    Initial Commit

---
+++ modals/script.js
@@ -0,0 +1,22 @@
+(function ($, w, d, h, b) {
+    $(document).ready(function () {
+        /**
+         * Construct.
+         */
+        function __construct() {
+
+        }
+
+        function modalShow(){
+            
+        }
+        
+        /**
+         * Instantiate
+         */
+        __construct();
+    });
+    $(w).on('load', function () {
+        $('body').addClass('siteloaded');
+    });
+})(jQuery, window, document, 'html', 'body');
